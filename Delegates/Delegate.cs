116f9bd28eb8f86965cd699a218d60fa1ab0c498
BuLbl4 <113981565+BuLbl4@users.noreply.github.com>
2023-03-23 13:04:41 +0300

    Add files via upload

---
+++ Delegates/Delegate.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public class Delegate<T>
+    {
+        public new List<T> list;
+        public Delegate(List<T> list)
+        {
+            this.list= list;
+        }
+
+
+        public int Count(Func<T,bool> func)
+        {
+            //return list.Count(func);
+            int count = 0;  
+            foreach (var item in list)
+            {
+                if(func(item)) 
+                    count++;
+            }
+            Console.WriteLine(count);
+            return count;
+        }
+        public T1? Aggregate1<T1>(Func<T,T1?,T1> func) 
+        {
+            var temp = default(T1);
+            foreach(var item in list)
+            {
+                temp = func(item,temp);
+            }
+            Console.WriteLine(temp);
+            return temp;
+        }
+        public T1? Aggregate2<T1>(Func<T,T1,T1> func, T1 defval)
+        {
+            foreach (var item in list) 
+            {
+                defval = func(item,defval);
+            }
+            Console.WriteLine(defval);
+            return defval;
+        }
+        public override string ToString()
+        {
+            foreach (var i in list)
+                Console.Write($"{i} ");
+            return "";
+        }
+
+
+
+    }
+}
---
+++ Delegates/Program.cs
@@ -0,0 +1,48 @@
+﻿
+using Delegates;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> t = new List<int> { 1, 2 ,3};
+        var dt = new Delegate<int>(t);
+        Console.WriteLine(dt);   
+        //dt.Aggregate2<int>((x, y) => x + y, 0);
+        dt.Aggregate2<int>((x,y) =>  x,0);
+        dt.Aggregate2<int>((x, y) => y, 0);
+        dt.Aggregate2<int>((x, y) => x * 2 + y * 2, 0);
+        dt.Aggregate2<int>((x, y) => x * 2 + y, 0);
+        Console.WriteLine("_________________");
+
+
+
+        List<int> q = new List<int> { 1, 2, 3, 4, 1000, 2424, 21,-1,3 };
+        var dq = new Delegate<int>(q);
+        Console.WriteLine(dq);
+        dq.Aggregate2<int>((x,y) => Math.Max(x,y),0);
+        dq.Aggregate2<int>((x, y) => Math.Min(x, y), 0);
+        Console.WriteLine("_________________");
+
+
+        List<string> str = new List<string> { "B", "u", "l", "a", "t" };
+        var dstr = new Delegate<string>(str);
+        Console.WriteLine(dstr);
+        dstr.Aggregate2<string>((x,y) => y + x,"");
+        dstr.Aggregate2<string>((x, y) => x + y, "");
+        Console.WriteLine("_________________");
+
+
+        var list = new List<int> { -1,-2,-3,-10,0,1,2,3,4,5,6,7,8};
+        var dlist = new Delegate<int>(list);
+        dlist.Count(x => x > 0);
+        dt.Count(x => x % 2 == 0);
+        dt.Count(x => x % 2 == 1);
+        dt.Count(x => x % 2 != 0 && x > 5);
+
+        //dt.Aggregate1<int>((x, y) => x + y);
+
+    }
+}
+
+
