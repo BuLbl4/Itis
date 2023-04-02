d6661e8af68620fde1f1bad1caf811d3daab3583
BuLbl4 <113981565+BuLbl4@users.noreply.github.com>
2023-04-02 22:03:28 +0300

    Add files via upload

---
+++ Semestrovka/Program.cs
@@ -0,0 +1,92 @@
+﻿using Semestrovka;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        WriteInFile();
+        //var r = new Random();
+        //int[] t = new int[100000];
+        //for(int i = 0; i < t.Length; i++)
+        //{
+        //    t[i] = r.Next(-1000,1000);  
+        //}
+        //Shellsort(t);
+    }
+    static void WriteInFile()
+    {
+        int pcount = 0;
+        var r = new Random();
+        for (int i = 1; i < 51; i++)
+        {
+            pcount += 10000;
+            int[] ints = new int[pcount];
+            using (var sw = new StreamWriter($@"C:\Sem\Set{i}.txt",false))
+            {
+                for (int j = 0; j < pcount; j++)
+                {
+                    ints[j] = r.Next(-10000, 10000);
+                    sw.Write($"{ints[j]} ");
+
+                }
+            }
+        }
+    }
+    static void ReadFile()
+    {
+        int qcount = 0;
+        for (int q = 1; q < 51; q++)
+        {
+            qcount += 10000;
+            int[] ints = new int[qcount];
+            using (StreamReader sr = new StreamReader($@"C:\Sem\Set{q}.txt"))
+            {
+                while (sr.Peek() != -1)
+                {
+                    string[] str = sr.ReadLine().Split(' ');
+                    for (int i = 0; i < str.Length - 1; i++)
+                    {
+                        ints[i] = int.Parse(str[i]);
+                    }
+                    Stopwatch watch = new Stopwatch();
+                    watch.Start();
+                    Shellsort(ints);
+                    watch.Stop();
+                   // Console.WriteLine(watch.ElapsedMilliseconds);
+                }
+            }
+        }
+    }
+    static void Shellsort(int[] array)
+    {
+        int iteration = 0;
+        var d = array.Length / 2;
+        while (d >= 1)
+        {
+            for (var i = d; i < array.Length; i++)
+            {
+                var j = i;
+                while ((j >= d) && (array[j - d] > array[j]))
+                {
+                    (array[j], array[j - d]) = (array[j - d], array[j]);
+                    j = j - d;
+                    iteration++;
+                }
+            }
+            d = d / 2;
+        }
+
+    }
+}
+
+
+
+
+
---
+++ Semestrovka/ShellSort.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semestrovka
+{
+    public class ShellSort
+    {
+        public List<int[]> list;
+        public ShellSort(List<int[]> lists)
+        {
+            list = lists;
+        }
+        public void Sort() 
+        {
+            for(int i = 0; i < list.Count; i++) 
+            {
+                
+            }
+        }
+        public int[] Shellsort(int[] lists)
+        {
+            
+            int iteration = 0;
+            var d = lists.Length / 2;
+            while (d >= 1)
+            {
+                for (var i = d; i < lists.Length; i++)
+                {
+                    var j = i;
+                    while ((j >= d) && (lists[j - d] > lists[j]))
+                    {
+                        (lists[j], lists[j - d]) = (lists[j - d], lists[j]);
+                        j = j - d;
+                        iteration++;
+                    }
+                }
+                d = d / 2;
+            }
+            Console.WriteLine(iteration);
+            return lists;
+        }
+
+        public override string ToString()
+        {
+            for(int i = 0; i < list.Count;i++)
+                Console.WriteLine(list[i]);
+            return "";
+        }
+    }
+}
